2b677b0e65db739498a429c1e6468b121fb4dc49
yoondae <yoondae88@naver.com>
2025-07-04 17:15:31 +0900

    첫 번째 커밋: index.php 파일 추가

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>나의 첫 게시판</title>
+</head>
+<body>
+    <h1><?php echo "Hello, World! 나의 첫 게시판에 오신 것을 환영합니다!"; ?></h1>
+</body>
+</html>
